a0f53ed1909f102d06b6f123c2b0c36e4eb76987
Ewa Margas-Nowak <101837401+EwaMN@users.noreply.github.com>
2022-08-10 13:31:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery")
+const galleryItem = galleryItems.map((galleryItem) =>
+`
+  <a class="gallery__item" href="${galleryItem.original}">
+    <img
+      class="gallery__image" src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+      />
+  </a>`).join("");
+
+
+gallery.innerHTML = galleryItem;
+
+/*gallery.insertAdjacentHTML("afterbegin", galleryItem);*/
+
+new SimpleLightbox (".gallery a", {captionDelay:250})
+;
 console.log(galleryItems);
