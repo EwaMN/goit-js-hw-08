7427c98c9ccdb54d146b78faeac7bc2a213361e6
Ewa Margas-Nowak <101837401+EwaMN@users.noreply.github.com>
2022-08-10 13:31:29 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on("timeupdate", throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`);
+
+}, 1000));
+
+const actualTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(actualTime).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });
